5201b09f289ec5613a32cf3b9e322344abb774b2
mark langan <markelangan@gmail.com>
2023-04-26 14:10:53 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 1
+let num2 = 62
+let multiply = num1 * num2
+console.log(multiply)
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1
+console.log(random)
+
+let num3 = 8
+let num4 = 2
+let mod = num3/num4
+console.log(mod)
+
+let max = Math.max(2,4,6,8,20)
+console.log(max)
